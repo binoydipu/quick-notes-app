cac44f5d0e3758d7921cd8dd9960ec9e111c8d40
Binoy Barman <binoydipu@gmail.com>
2024-09-17 11:55:13 +0600

    step-8

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,38 @@
+import 'package:dummy/services/auth/auth_provider.dart';
+import 'package:dummy/services/auth/auth_user.dart';
+
+// to expose the given AuthProvider to the UI and can hold additional logic
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+
+  const AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) =>
+      provider.createUser(
+        email: email,
+        password: password,
+      );
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) =>
+      provider.logIn(
+        email: email,
+        password: password,
+      );
+
+  @override
+  Future<void> logOut() => provider.logOut();
+
+  @override
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
+}
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -64,10 +64,20 @@ class _LoginViewState extends State<LoginView> {
                   password: password,
                 );
                 if (context.mounted) {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route) => false,
-                  );
+                  final user = FirebaseAuth.instance.currentUser;
+                  if(user?.emailVerified ?? false) {
+                    // user's email is VERIFIED
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      notesRoute,
+                      (route) => false,
+                    );
+                  } else {
+                    // user's email is NOT verified
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute,
+                      (route) => false,
+                    );
+                  }
                 }
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
